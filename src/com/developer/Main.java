c4eb32ce0045dee7138dc21e7f0004136a89cfe1
faz_ri <77220176+fazriridwan19@users.noreply.github.com>
2022-05-13 18:09:38 +0700

    minor update

--- src/com/developer/Main.java
+++ src/com/developer/Main.java
@@ -1,32 +1,21 @@
 package com.developer;
 
 import java.io.IOException;
-import java.sql.SQLOutput;
 import java.util.Scanner;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, CsvException {
-        FCM cmean = new FCM();
+    public static void main(String[] args) throws IOException {
+        FCM cmean = new FCM("data_lulus_tepat_waktu.csv");
 
         //get number of class from user
         System.out.println("Please input number of cluster that you want :");
         Scanner sc= new Scanner(System.in);
         String read1 = sc.nextLine();
 
-        //read data
-        cmean.readData();
-
         //run clustering algorithm
-        cmean.run(Integer.parseInt(read1), 100, cmean.data);
+        cmean.run(Integer.parseInt(read1), 100);
 
-        //write cluster center to file
-        cmean.writeDataToFile(cmean.clusterCenters, "cluster_centers");
-
-        //write data cluster to file
-        cmean.writeClusterToFile(cmean.data, "data_cluster");
 
         System.out.println("Clustering Finished!!!");
 
