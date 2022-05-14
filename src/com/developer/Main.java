7dbd4fde59ce90f363881f7c1444ecf27ee00605
faz_ri <77220176+fazriridwan19@users.noreply.github.com>
2022-05-14 10:40:52 +0700

    Add files via upload

--- src/com/developer/Main.java
+++ src/com/developer/Main.java
@@ -6,19 +6,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        FCM cmean = new FCM("data_lulus_tepat_waktu.csv");
+        FCM cmean = new FCM("data_lulus_tepat_waktu.csv", 2, 0.01);
 
         //get number of class from user
         System.out.println("Please input number of cluster that you want :");
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String read1 = sc.nextLine();
 
         //run clustering algorithm
         cmean.run(Integer.parseInt(read1), 100);
-
-
+        cmean.runTest("data_lulus_tepat_waktu_test.csv");
         System.out.println("Clustering Finished!!!");
-
-
     }
 }
